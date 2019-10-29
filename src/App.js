c8d73c672e6a84c5ba4563506c0f10bbf565fd7f
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-29 14:57:12 -0300

    feat: Hook useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <div>React Hooks</div>;
+  const [techs, setTechs] = useState([
+    'ReactJs',
+    'Nodejs',
+    'ReactNative',
+    'GraphQL',
+  ]);
+
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={event => setNewTech(event.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
